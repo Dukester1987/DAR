c564d3aa0bbadf60f9d8e570d2e7765518bed145
ldulka <lukas.dulka@gmail.com>
2016-10-11 16:20:22 +1100

    eDAR v 1.1.42 - Autoupdater

--- src/dar/remoteDB/DBWrapper.java
+++ src/dar/remoteDB/DBWrapper.java
@@ -6,22 +6,31 @@
 package dar.remoteDB;
 
 import dar.Functions.FileLogger;
+import dar.Functions.downloadManager;
+import dar.Functions.readXMLFile;
 import dar.Gui.Gui;
 import dar.localDB.LocalWraper;
 import java.awt.Color;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.*;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 
 public class DBWrapper implements Runnable{
     private static String USER;
     private static String PASSWORD;
     private static String CONN_STRING;
+    private static String ACTUAL_SW_VERSION;
+    private static String SERVER_ADDRESS;
+    private static String CHANGELOG_PATH;
     public Connection con;
     Thread thread;
     private boolean keepAlive = true;
@@ -40,6 +49,7 @@ public class DBWrapper implements Runnable{
     private long FAIL_RECONNECT;
     private long FAIL_TOTALRECONNECT;
     private long CHECKCONNECTION;
+    private boolean checkForUpdates = true;
 
     public DBWrapper(JLabel label, LocalWraper db, Gui g) {
         this.label = label;
@@ -111,12 +121,57 @@ public class DBWrapper implements Runnable{
         label.setForeground(Color.BLACK);
         label.setText("Connection to the remote database established");
         System.out.println("start sync");
+        if(checkForUpdates){            
+            try {
+                initproperties();
+                downloadManager dwnm = new downloadManager(SERVER_ADDRESS);
+                if(dwnm.download(new URL(SERVER_ADDRESS+"/UPDATE/changelog/version_manifest.xml"))){
+                    readXMLFile file = new readXMLFile();
+                    file.readFile("changelog/version_manifest.xml"); 
+                    if(!file.version.equals(ACTUAL_SW_VERSION)){
+                        //start updating
+                        int j = JOptionPane.showConfirmDialog(null, "There is a new version of Software available\nDo you want to update to version "+file.version+" now?", "new version "+file.version+"",JOptionPane.YES_NO_OPTION);
+                        if(j==0){ // yes update
+                            Runtime.getRuntime().exec("java -jar DAR_Updater.jar -runupdate");
+                            System.out.println("updating");                            
+                            System.exit(0);
+                        }
+                    }             
+                } 
+                checkForUpdates=false;
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                new FileLogger(ex.toString());
+            }
+        }
         startSync();        
         //keepAlive = false;
         keepAlive = putSleep(CHECKCONNECTION);
         label.setText("Checking connection");        
     }
 
+    private static void initproperties() {
+        FileInputStream input = null;
+        try {
+            input = new FileInputStream("./changelog/version_manifest.properties");        
+            Properties props = new Properties();
+            props.load(input);
+            ACTUAL_SW_VERSION = props.getProperty("app.version");
+            SERVER_ADDRESS = props.getProperty("app.address");
+            CHANGELOG_PATH = props.getProperty("app.changelog");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                input.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }    
+    
     private void startSync() {
         label.setText("getting list of changes");
         mgr = new ChangeManager(db, this);       
